7b63fee8f5e8e341ae784b8ee3e319ba9187b8bb
ShenYj <shenyanjie123@foxmail.com>
2016-10-21 11:31:57 +0800

    request data method

--- Weibo_Object-C/Weibo_Object-C/Common/JSNetworkTool.m
+++ Weibo_Object-C/Weibo_Object-C/Common/JSNetworkTool.m
@@ -7,6 +7,7 @@
 //
 
 #import "JSNetworkTool.h"
+#import "JSUserAccountTool.h"
 
 @implementation JSNetworkTool
 
@@ -23,6 +24,7 @@
     return _instanceType;
 }
 
+#pragma mark
 #pragma mark - Public
 
 - (void)requestWithMethod:(RequestMethod)requestMethod withParameters:(NSDictionary *)parametes withUrlString:(NSString *)urlString withSuccess:(void (^)(id obj))success withError:(void (^)(NSError *error))failure{
@@ -59,7 +61,7 @@
 }
 
 
-
+#pragma mark
 #pragma mark - Load access_token
 - (void)loadAccessTokenWithCode:(NSString *)code withFinishedBlock:(void (^)(id obj, NSError *error))finishedBlock {
     
@@ -95,6 +97,40 @@
         finishedBlock(obj,nil);
     } withError:^(NSError *error) {
         finishedBlock(nil,error);
+    }];
+    
+}
+
+#pragma mark - public content
+- (void)loadHomePublicDatawithFinishedBlock:(void (^)(id obj, NSError *error))finishedBlock{
+    
+    /*
+     
+     access_token	true	string	采用OAuth授权方式为必填参数，OAuth授权后获得。
+     since_id	false	int64	若指定此参数，则返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
+     max_id	false	int64	若指定此参数，则返回ID小于或等于max_id的微博，默认为0。
+     count	false	int	单页返回的记录条数，最大不超过100，默认为20。
+     page	false	int	返回结果的页码，默认为1。
+     base_app	false	int	是否只获取当前应用的数据。0为否（所有数据），1为是（仅当前应用），默认为0。
+     feature	false	int	过滤类型ID，0：全部、1：原创、2：图片、3：视频、4：音乐，默认为0。
+     trim_user	false	int	返回值中user字段开关，0：返回完整user字段、1：user字段仅返回user_id，默认为0。
+     
+     */
+    NSString *urlString = @"http://open.weibo.com/wiki/2/statuses/home_timeline";
+    NSDictionary *para = @{
+                           @"ccess_token": [JSUserAccountTool sharedManager].access_token,
+                           //@"since_id": @0,
+                           //@"max_id": @0,
+                           };
+    
+    [self requestWithMethod:RequestMethodGet withParameters:para withUrlString:urlString withSuccess:^(id obj) {
+        
+        finishedBlock(obj,nil);
+        
+    } withError:^(NSError *error) {
+        
+        finishedBlock(nil,error);
+        
     }];
     
 }
