f68f8ec68739ea98395d4095ea4ffa9b7807a5dc
ShenYj <shenyanjie123@foxmail.com>
2016-11-11 11:00:09 +0800

    重构

--- Weibo_Object-C/Weibo_Object-C/Classes/Home/Controllers/JSHomeTableViewController.m
+++ Weibo_Object-C/Weibo_Object-C/Classes/Home/Controllers/JSHomeTableViewController.m
@@ -155,11 +155,11 @@ static CGFloat const kPullDownLabelHeight = 34.f; // 下拉刷新展示更新多
             // 下拉刷新
             self.homeStatusDatas = [statusData arrayByAddingObjectsFromArray:self.homeStatusDatas];
             
+            // 显示更新多少条微博数据
+            [self pullDownAnimationWithStatusCounts:statusData.count];
         }
         
         [self.tableView reloadData];
-        // 显示更新多少条微博数据
-        [self pullDownAnimationWithStatusCounts:statusData.count];
         
     }];
     
@@ -214,7 +214,33 @@ static CGFloat const kPullDownLabelHeight = 34.f; // 下拉刷新展示更新多
         
     } completion:^(BOOL finished) {
         
-        [UIView animateWithDuration:1 animations:^{
+        /*
+         UIViewAnimationOptionLayoutSubviews            = 1 <<  0,
+         UIViewAnimationOptionAllowUserInteraction      = 1 <<  1, // turn on user interaction while animating
+         UIViewAnimationOptionBeginFromCurrentState     = 1 <<  2, // start all views from current value, not initial value
+         UIViewAnimationOptionRepeat                    = 1 <<  3, // repeat animation indefinitely
+         UIViewAnimationOptionAutoreverse               = 1 <<  4, // if repeat, run animation back and forth
+         UIViewAnimationOptionOverrideInheritedDuration = 1 <<  5, // ignore nested duration
+         UIViewAnimationOptionOverrideInheritedCurve    = 1 <<  6, // ignore nested curve
+         UIViewAnimationOptionAllowAnimatedContent      = 1 <<  7, // animate contents (applies to transitions only)
+         UIViewAnimationOptionShowHideTransitionViews   = 1 <<  8, // flip to/from hidden state instead of adding/removing
+         UIViewAnimationOptionOverrideInheritedOptions  = 1 <<  9, // do not inherit any options or animation type
+         
+         UIViewAnimationOptionCurveEaseInOut            = 0 << 16, // default
+         UIViewAnimationOptionCurveEaseIn               = 1 << 16,
+         UIViewAnimationOptionCurveEaseOut              = 2 << 16,
+         UIViewAnimationOptionCurveLinear               = 3 << 16,
+         
+         UIViewAnimationOptionTransitionNone            = 0 << 20, // default
+         UIViewAnimationOptionTransitionFlipFromLeft    = 1 << 20,
+         UIViewAnimationOptionTransitionFlipFromRight   = 2 << 20,
+         UIViewAnimationOptionTransitionCurlUp          = 3 << 20,
+         UIViewAnimationOptionTransitionCurlDown        = 4 << 20,
+         UIViewAnimationOptionTransitionCrossDissolve   = 5 << 20,
+         UIViewAnimationOptionTransitionFlipFromTop     = 6 << 20,
+         UIViewAnimationOptionTransitionFlipFromBottom  = 7 << 20,
+         */
+        [UIView animateWithDuration:1 delay:0.25 options:UIViewAnimationOptionAllowUserInteraction animations:^{
             
             self.pullDownStatusCountsLabel.transform = CGAffineTransformIdentity;
             
